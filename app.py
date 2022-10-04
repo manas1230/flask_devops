956d556fd96e139f9c68385a9fbc6b5f28246230
Manas <54947829+manas1230@users.noreply.github.com>
2022-10-04 21:05:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-from sklearn.externals import joblib
+import joblib
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.preprocessing import StandardScaler
