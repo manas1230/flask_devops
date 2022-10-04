5faf024e306bb6cb9459bb0fd49aaf646493f4a4
Manas <54947829+manas1230@users.noreply.github.com>
2022-10-04 22:56:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
-
+import pickle
 import pandas as pd
-from sklearn.externals import joblib
+import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
