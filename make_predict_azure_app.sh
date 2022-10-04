3ba07605ff788adfd30081d16a09e9336d4e47a6
Manas <54947829+manas1230@users.noreply.github.com>
2022-10-04 22:14:30 +0530

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://asd34fd56my.azurewebsites.net:$PORT/predict 
+     -X POST https://abcd123m.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
