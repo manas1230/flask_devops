dc982e62df2adb7dcfba536f719aae05548927dd
Manas <54947829+manas1230@users.noreply.github.com>
2022-10-04 22:36:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,6 +66,7 @@ def predict():
     LOG.info("inference payload DataFrame: %s inference_payload")
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
+    prediction = [int(x) for x in prediction]
     return jsonify({'prediction': prediction})
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
