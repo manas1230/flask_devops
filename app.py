ca691e6ff1573904291e122b01b0b16391b6bf22
Manas <54947829+manas1230@users.noreply.github.com>
2022-10-04 23:00:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def predict():
     """
    
     try:
-        clf = joblib.load("boston_housing_prediction.joblib")
+        clf = pickle.load("boston_housing_prediction.joblib")
     except Exception as e:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
