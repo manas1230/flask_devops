f1056bc580df9483681858fd4de4847249b103f7
Manas <54947829+manas1230@users.noreply.github.com>
2022-10-06 01:54:43 +0530

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,1 +1,3 @@
-az webapp up --name asd34fd56my --resource-group Azuredevops --runtime "PYTHON:3.7"
+az webapp up --name abcd123m --resource-group Azuredevops --runtime "PYTHON:3.7"
+
+#az webapp up --name asd34fd56my --resource-group Azuredevops --runtime "PYTHON:3.7"
