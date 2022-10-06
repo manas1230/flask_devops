2578e35e9b4d7a1e86f35b87e757f46892bcf264
Manas <54947829+manas1230@users.noreply.github.com>
2022-10-06 20:30:41 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
 from locust import HttpUser, between, task
 
 class WebsiteUser(HttpUser):
-    #host = "http://abcd123m.azurewebsites.net"
+    host = "http://abcd123m.azurewebsites.net"
     wait_time = between(5, 15)
 
     @task
